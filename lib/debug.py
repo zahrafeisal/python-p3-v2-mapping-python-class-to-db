dff141c34ece63e9f67c0b7889f7f8fe7f0b3631
zahrafeisal <zahrafeisal3@gmail.com>
2025-02-03 21:13:01 +0300

    ORM

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,34 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+# payroll = Department("Payroll", "Building A, 5th Floor")
+# print(payroll)
+
+# payroll.save()     Persist to db, assign object id attribute
+# print(payroll)
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+# hr = Department("Human Resources", "Building C, East Wing")
+# print(hr)
+
+# hr.save()          Persist to db, assign object id attribute
+# print(hr)
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+hr.name = "HR"
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+print("delete payroll")
+payroll.delete()
+print(payroll)
+
 ipdb.set_trace()
